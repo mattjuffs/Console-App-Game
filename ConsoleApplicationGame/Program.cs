ff879b07330f46ed0be10e51c4bc8aa02a1dd9d6
Matt Juffs <matt@slickhouse.com>
2017-09-22 08:38:24 +0100

    Initial commit

---
+++ ConsoleApplicationGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationGame
+{
+    class Program
+    {
+        private static void RenderText(string text)
+        {
+            char[] characters = text.ToCharArray();
+
+            foreach (var character in characters)
+            {
+                Console.Write(character);
+
+                if (character == ',')
+                    System.Threading.Thread.Sleep(200);
+                else
+                    System.Threading.Thread.Sleep(100);
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void Welcome()
+        {
+            string welcomeText = "Hello, what is your name?";
+            RenderText(welcomeText);
+        }
+
+        private static void SayHello()
+        {
+            string name = Console.ReadLine();
+            string helloText = string.Format("Hello {0}, I am computer!", name);
+            RenderText(helloText);
+        }
+
+        static void Main(string[] args)
+        {
+            Welcome();
+            SayHello();
+            Console.Read();
+            return;
+        }
+    }
+}
